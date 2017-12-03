cd486e40d5179e285876b8b109600c5df633b352
Sebastien Alaiwan <Unknown>
2017-12-03 17:42:30 +0100

    More rooms

--- src/resources.cpp
+++ src/resources.cpp
@@ -37,7 +37,7 @@ static const Resource models[] =
   { MDL_ROOMS + 0, "res/rooms/room-00.json" },
   { MDL_ROOMS + 1, "res/rooms/room-01.json" },
   { MDL_ROOMS + 2, "res/rooms/room-02.json" },
-  // { MDL_ROOMS+3, "res/rooms/room-03.json" },
+  { MDL_ROOMS + 3, "res/rooms/room-03.json" },
   // { MDL_ROOMS+4, "res/rooms/room-04.json" },
   // { MDL_ROOMS+5, "res/rooms/room-05.json" },
   // { MDL_ROOMS+6, "res/rooms/room-06.json" },
