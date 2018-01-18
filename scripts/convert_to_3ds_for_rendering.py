6e37ebe5829e4a2710a94c850fe47b5a481b515d
Sebastien Alaiwan <Unknown>
2018-01-18 22:59:38 +0100

    When baking the lightmap, take into account the existing textures

--- scripts/convert_to_3ds_for_rendering.py
+++ scripts/convert_to_3ds_for_rendering.py
@@ -12,13 +12,15 @@ output3dsPath = argv[0]
 outputPngPath = argv[1]
 
 def run():
-  bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+  bpy.ops.object.mode_set(mode='OBJECT')
 
   #----------------------------------------
   bpy.ops.object.select_by_type(type='MESH')
   bpy.ops.object.convert(target='MESH')
 
+  #----------------------------------------------------------------------------
   # join everything into one single object
+  #----------------------------------------------------------------------------
   if len(bpy.context.selected_objects) > 1:
     bpy.ops.object.join()
 
@@ -31,6 +33,9 @@ def run():
 
   assert(len(bpy.data.meshes) == 1)
   ob = bpy.data.meshes[0]
+  ob.uv_textures[0].name = "TextureUV"
+  bpy.ops.mesh.uv_texture_add()
+  ob.uv_textures[1].name = "AtlasUV"
 
   # apply all transforms
   bpy.ops.object.transform_apply(location=True, scale=True, rotation=True)
@@ -43,21 +48,27 @@ def run():
   bpy.data.worlds["World"].light_settings.ao_factor=1.0
   bpy.data.worlds["World"].light_settings.use_environment_light=True
   bpy.data.worlds["World"].light_settings.environment_energy=0.1
-  bpy.data.worlds["World"].light_settings.samples = 10
+  bpy.data.worlds["World"].light_settings.samples = 5
 
+  #----------------------------------------------------------------------------
   # baking
-  bpy.ops.object.mode_set(mode='EDIT', toggle=False)
+  #----------------------------------------------------------------------------
+  bpy.ops.object.mode_set(mode='EDIT')
   bpy.ops.mesh.select_all(action='SELECT')
-  bpy.ops.uv.lightmap_pack(PREF_NEW_UVLAYER=True)
 
   # Create new image
-  image = bpy.ops.image.new(name="myLightMap", width=2048, height=2048)
+  bpy.ops.image.new(name="myLightMap", width=2048, height=2048)
+
+  # Compute lightmap UV coords
+  bpy.ops.uv.lightmap_pack()
 
   # Set bake margins
   bpy.context.scene.render.bake_margin = 2
 
   # Set the UV coordinates
-  ob.uv_textures['UVMap'].active = True
+  ob.uv_textures['TextureUV'].active_render = True
+  ob.uv_textures['AtlasUV'].active_render = False
+  ob.uv_textures.active_index = 1
 
   # Specify the bake type
   bpy.data.scenes["Scene"].render.bake_type = "FULL"
@@ -66,7 +77,7 @@ def run():
   bpy.ops.object.mode_set(mode='OBJECT')
 
   # Set the target image
-  for d in ob.uv_textures['UVMap'].data:
+  for d in ob.uv_textures['AtlasUV'].data:
      d.image = bpy.data.images["myLightMap"]
 
   # Enter edit mode
@@ -77,7 +88,17 @@ def run():
   bpy.ops.object.bake_image()
 
   bpy.data.images["myLightMap"].save_render(outputPngPath, bpy.context.scene)
+
+  # Set the UV coordinates
+  ob.uv_textures['TextureUV'].active_render = False
+  ob.uv_textures['AtlasUV'].active_render = True
+  ob.uv_textures.active_index = 1
+
+  # bpy.ops.wm.save_mainfile(filepath="/tmp/debug.blend")
+
   my_3ds_exporter.saveAs3ds(bpy.context.scene, output3dsPath)
+
+  # bpy.ops.wm.save_mainfile(filepath="/tmp/debug.blend")
 
 try:
   run()
