afe7ca46ab6ba861cf422a4c311f65c4f18ad290
Sebastien Alaiwan <Unknown>
2018-08-10 08:39:00 +0200

    Shader: add comments

--- engine/src/fragment.glsl
+++ engine/src/fragment.glsl
@@ -1,9 +1,13 @@
 #version 100
+
 precision mediump float;
+
+// Interpolated values from the vertex shader
 varying vec2 v_texCoord;
 varying vec3 vNormal;
 varying float fogFactor;
 
+// Values that stay constant for the whole mesh
 uniform vec4 v_color;
 uniform sampler2D s_baseMap;
 uniform vec3 ambientLight;
--- engine/src/vertex.glsl
+++ engine/src/vertex.glsl
@@ -1,13 +1,16 @@
 #version 100
 
+// Input vertex data, different for all executions of this shader
 attribute vec4 a_position;
 attribute vec2 a_texCoord;
 attribute vec3 a_normal;
 
+// Output data; will be interpolated for each fragment
 varying vec2 v_texCoord;
 varying vec3 vNormal;
 varying float fogFactor;
 
+// Values that stay constant for the whole mesh.
 uniform mat4 MVP;
 
 void main()
