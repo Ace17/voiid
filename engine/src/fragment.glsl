b5f3667bf2935a3fe46bf2a3941ff009f97ed439
Sebastien Alaiwan <Unknown>
2018-09-27 19:12:22 +0200

    Use GLSL langage 3.00 (as in deeep)

--- engine/src/fragment.glsl
+++ engine/src/fragment.glsl
@@ -1,11 +1,14 @@
-#version 100
+#version 300 es
 
 precision mediump float;
 
 // Interpolated values from the vertex shader
-varying vec2 v_texCoord;
-varying vec3 vNormal;
-varying float fogFactor;
+in vec2 v_texCoord;
+in vec3 vNormal;
+in float fogFactor;
+
+// Ouput data
+out vec4 color;
 
 // Values that stay constant for the whole mesh
 uniform vec4 v_color;
@@ -18,8 +21,8 @@ void main()
   vec3 diffuseLight = vec3(max(0.0, dot(lightDirEyeSpace, vNormal)));
   vec3 light = diffuseLight + ambientLight;
   light = light * fogFactor;
-  vec4 color = texture2D(s_baseMap, v_texCoord) + v_color;
-  gl_FragColor = vec4(color.xyz * light, color.a);
+  vec4 rawColor = texture2D(s_baseMap, v_texCoord) + v_color;
+  color = vec4(rawColor.xyz * light, rawColor.a);
 }
 
 // vim: syntax=glsl
--- engine/src/vertex.glsl
+++ engine/src/vertex.glsl
@@ -1,14 +1,14 @@
-#version 100
+#version 300 es
 
 // Input vertex data, different for all executions of this shader
-attribute vec4 a_position;
-attribute vec2 a_texCoord;
-attribute vec3 a_normal;
+in vec4 a_position;
+in vec2 a_texCoord;
+in vec3 a_normal;
 
 // Output data; will be interpolated for each fragment
-varying vec2 v_texCoord;
-varying vec3 vNormal;
-varying float fogFactor;
+out vec2 v_texCoord;
+out vec3 vNormal;
+out float fogFactor;
 
 // Values that stay constant for the whole mesh.
 uniform mat4 MVP;
