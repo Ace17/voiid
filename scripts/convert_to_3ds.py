b1c9dd50c5f5a3e688008ddad795f2d927754eb2
Sebastien Alaiwan <Unknown>
2019-08-18 12:45:59 +0200

    Cleanup dead code

--- scripts/convert_to_3ds.py
+++ scripts/convert_to_3ds.py
@@ -17,16 +17,6 @@ def run():
   bpy.context.scene.objects.active = bpy.data.objects[0]
   bpy.ops.object.convert(target='MESH')
   
-  print("keys before: ", len(bpy.context.scene.objects.keys()))
- 
-#    # join everything into one single object
-#    if len(bpy.context.scene.objects.keys()) > 1:
-#      bpy.ops.object.join()
-#
-#    print("keys after: ", len(bpy.context.scene.objects.keys()))
-
-#    bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
-
   # apply all transforms
   bpy.ops.object.select_all(action='SELECT')
   bpy.ops.object.transform_apply(location=True, scale=True, rotation=True)
