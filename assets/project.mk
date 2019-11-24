7fcbc5e5c2d38d2f04ba6d857d79b04d2753616e
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2019-11-24 14:43:39 +0100

    Rename 'res-src' -> 'assets'

---
+++ assets/project.mk
@@ -0,0 +1,39 @@
+SOUNDS_SRC+=$(wildcard assets/sounds/*.ogg)
+SOUNDS_SRC+=$(wildcard assets/music/*.ogg)
+TARGETS+=$(SOUNDS_SRC:assets/%.ogg=res/%.ogg)
+
+res/%.ogg: assets/%.ogg
+	@mkdir -p $(dir $@)
+	@echo "Transcode $<"
+	@ffmpeg -loglevel 1 -y -i "$<" -ar 22050 -ac 2 "$@" </dev/null
+
+ROOMS_SRC+=$(wildcard assets/rooms/*/mesh.blend)
+TARGETS+=$(ROOMS_SRC:assets/%.blend=res/%.mesh)
+TARGETS+=$(ROOMS_SRC:assets/%.blend=res/%.render)
+
+SPRITES_SRC+=$(wildcard assets/sprites/*.blend)
+TARGETS+=$(SPRITES_SRC:assets/%.blend=res/%.render)
+TARGETS+=$(SPRITES_SRC:assets/%.blend=res/%.png)
+
+TARGETS+=res/font.png
+res/font.png: assets/font.png
+
+res/%.sa.blend: assets/%.blend ./scripts/preprocess_blender.py
+	@mkdir -p $(dir $@)
+	@echo "Preprocess blender file: $<"
+	@./scripts/preprocess_blender "$<" "$@"
+
+res/%.mesh: res/%.sa.blend ./scripts/export_from_blender.py
+	@mkdir -p $(dir $@)
+	@echo "Exporting from blender: $<"
+	@./scripts/export_from_blender "$<" "$@"
+
+res/%.render: res/%.mesh $(BIN)/meshcooker.exe
+	@mkdir -p $(dir $@)
+	@cp assets/$*.png res/$*.diffuse.png
+	$(BIN)/meshcooker.exe "$<" "$@" "res/$*.lightmap.png"
+
+res/%: assets/%
+	@mkdir -p $(dir $@)
+	@cp "$<" "$@"
+
