76753bbba4a6ce1d5abd58fbe41f4edff9f343ee
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2020-05-09 11:20:58 +0200

    Audio: reduce latency

--- engine/src/audio/audio_sdl.cpp
+++ engine/src/audio/audio_sdl.cpp
@@ -38,7 +38,7 @@ struct SdlAudioBackend : IAudioBackend
     desired.freq = 22050;
     desired.format = AUDIO_F32SYS;
     desired.channels = 2;
-    desired.samples = 4096; // workaround slowness on chrome causing dropouts
+    desired.samples = 512;
     desired.callback = &staticMixAudio;
     desired.userdata = this;
 
