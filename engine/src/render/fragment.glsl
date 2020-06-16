668ab2e284163d113556724a3705772a5df8d231
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2020-06-16 14:12:40 +0200

    Fix lighting

--- engine/src/render/fragment.glsl
+++ engine/src/render/fragment.glsl
@@ -20,16 +20,31 @@ uniform vec3 ambientLight;
 
 void main()
 {
+  vec3 lightColor = vec3(1,1,1);
   vec3 lightPos = vec3(2, 2, 2);
-  vec3 lightDir = lightPos - vPos.xyz;
+  vec3 lightDir = lightPos - vPos;
   float lightDist = length(lightDir);
-  float attenuation = 1.0/lightDist;
+  float attenuation = 10.0/(lightDist*lightDist);
   vec3 receivedLight = vec3(max(0.0, dot(normalize(lightDir), vNormal))) * attenuation;
-  vec3 light = receivedLight + ambientLight * 0.2;
-  light *= fogFactor;
-  vec4 rawColor = texture2D(DiffuseTex, UV) + fragOffset;
-  vec4 lightmapColor = texture2D(LightmapTex, UV_lightmap);
-  color = vec4(rawColor.rgb * (light + lightmapColor.rgb*0.01), rawColor.a);
+
+  // ambient
+  vec3 ambient = lightColor * ambientLight;
+
+  // diffuse
+  float diff = max(0.0, dot(normalize(lightDir), vNormal))*attenuation;
+  diff += length(texture2D(LightmapTex, UV_lightmap)) * 0.01;
+  diff *= fogFactor;
+  vec3 diffuse = lightColor * (diff * (texture2D(DiffuseTex, UV).rgb + fragOffset.rgb));
+
+  // specular
+  // vec3 viewDir = normalize(viewPos - vPos);
+  // vec3 reflectDir = reflect(-normalize(lightDir), vNormal);
+  // float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+  // vec3 specular = lightColor * (spec * material.specular);
+  vec3 specular = vec3(0);
+
+  vec3 result = ambient + diffuse + specular;
+  color = vec4(result, texture2D(DiffuseTex, UV).a);
 
   if(false)
   {
