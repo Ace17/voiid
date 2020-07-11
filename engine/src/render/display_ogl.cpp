86452c8dc64c20fc7e0045e53de2cc3855aa0415
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2020-07-11 19:08:44 +0200

    Enable mipmapping

--- engine/src/render/display_ogl.cpp
+++ engine/src/render/display_ogl.cpp
@@ -215,7 +215,8 @@ GLuint uploadTextureToGPU(const Picture& pic)
 
   glBindTexture(GL_TEXTURE_2D, texture);
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, pic.dim.width, pic.dim.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pic.pixels.data());
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  SAFE_GL(glGenerateMipmap(GL_TEXTURE_2D));
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
