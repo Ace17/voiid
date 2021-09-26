c771cbbf97bf006eaff042291b6de3236a007bb4
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2021-09-26 10:59:28 +0200

    Bump copyright year

--- src/render/png.cpp
+++ src/render/png.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2018 - Sebastien Alaiwan
+// Copyright (C) 2021 - Sebastien Alaiwan
 // Copyright (c) 2005-2010 Lode Vandevenne
 //
 // This software is provided 'as-is', without any express or implied
