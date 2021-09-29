0f28b5089429c067ef43901155610ab0086910d1
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2021-09-29 08:18:18 +0200

    Add comments

--- src/engine/display.h
+++ src/engine/display.h
@@ -4,6 +4,8 @@
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
 
+// interface to a high-level renderer
+
 #pragma once
 
 #include <stdint.h>
