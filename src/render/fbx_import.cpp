0ad7c134facf622a19b89c734b66d525f2ecbfbd
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2022-03-27 22:42:04 +0200

    Add comments

--- src/render/fbx_import.cpp
+++ src/render/fbx_import.cpp
@@ -1,3 +1,11 @@
+// Copyright (C) 2022 - Sebastien Alaiwan
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+
+// Loader for the FBX file format
+
 #include "base/error.h"
 #include "base/matrix4.h"
 #include "base/mesh.h"
